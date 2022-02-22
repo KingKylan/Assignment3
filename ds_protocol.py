7eeb71d40c178f643bca9990679d4883d9bf0fd7
KingKylan <97084093+KingKylan@users.noreply.github.com>
2022-02-21 19:55:35 -0800

    Update ds_protocol.py

--- ds_protocol.py
+++ ds_protocol.py
@@ -1,32 +1,24 @@
-# ds_protocol.py
-
-# Starter code for assignment 3 in ICS 32 Programming with Software Libraries in Python
-
-# Replace the following placeholders with your information.
-
-# NAME
-# EMAIL
-# STUDENT ID
-
 import json
 from collections import namedtuple
 
-# Namedtuple to hold the values retrieved from json messages.
-# TODO: update this named tuple to use DSP protocol keys
-DataTuple = namedtuple('DataTuple', ['foo', 'baz'])
 
+
+DataTuple = namedtuple('response', ['type','message', 'token'])
+
+# takes the json message and breaks it back down to a named tuple for easier use
 
 def extract_json(json_msg: str) -> DataTuple:
-    '''
-    Call the json.loads function on a json string and convert it to a DataTuple object
 
-    TODO: replace the pseudo placeholder keys with actual DSP protocol keys
-    '''
     try:
         json_obj = json.loads(json_msg)
-        foo = json_obj['foo']
-        baz = json_obj['bar']['baz']
+        response = json_obj['response']
+        type = response['type']
+        message = response['message']
+        try:
+            token = response['token']
+        except:
+            token = ''
     except json.JSONDecodeError:
         print("Json cannot be decoded.")
 
-    return DataTuple(foo, baz)
+    return DataTuple(type, message, token)
