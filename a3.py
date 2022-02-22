1eda33330d8f1332885baf2d47619233d807efc7
zarqus99 <grimaldiervin@gmail.com>
2022-02-21 22:21:45 -0800

    Update a3.py

--- a3.py
+++ a3.py
@@ -14,18 +14,25 @@ from ui import *
 import manager
 import local_handle
 
-def join(p):
-    print('QUESTA')
-    print(p)
-    nome = local_handle.profile1.username
-    print(nome)
+
+'''
+PROGRAM OUTILINE-MAP
+(a3 <-> ui)     #This is where the program starts. When started, direct to run function in manager.py
+    | |
+   manager [run]    #here the user-command input is taken and tested the validity. Once valid the command is processed
+    | |
+   Process_Path     #The input get splited in minor section and used for comands. Splits occurs through a combination of 
+    | | 
+   local_handle     #Journal's function resides here. This file has the role of managing the user's Profile and give the change to post online or save only localy
+'''
+
 
 # This function riderect the user input to the proper function
-def command(cmd, path, operations, name='', servo=False):
-    global admin    #delcare globally admin, useful for UI
+def command(cmd, path, operations, name='', servo=False):   # cmd is the command, path is the path to the local .dsu file, operations are the set of commands for the visualization of local file managment, name is the name of the file that the user want to create
+    global admin    # delcare globally admin, useful for UI. Servo has the role to check if admin mode is active or not
     admin = servo
 
-    dict = {'-usr': '', '-bio': '', '-pwd': '', '-posts': '', '-post': '', '-addpost': [], '-delpost': ''}  #set to empty the values
+    dict = {'-usr': '', '-bio': '', '-pwd': '', '-posts': '', '-post': '', '-addpost': [], '-delpost': ''}  #set to empty the values. This allow to not create duplicate and let the function work
     if cmd == 'L':
         if '-r' not in operations:
             if not admin:
@@ -47,14 +54,13 @@ def command(cmd, path, operations, name='', servo=False):
         local_handle.edit(operations, dict)
     elif cmd == 'P':
         local_handle.output_print(operations, dict)
-    elif cmd == 'join':
-        join(path)
+
     else:
         print(error)
-        manager.run()
+        manager.run()   # In case of error, Loop back for new user's input
 
 
 # Program begins
 if __name__ == '__main__':
     print(menu)
-    manager.run()   # Direct to a function ru, which manage the initial input and is the backbone of the program
+    manager.run()   # Direct to a function run, which manage the initial input and is the backbone of the program
